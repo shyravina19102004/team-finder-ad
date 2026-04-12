1cde8733658402a2955177e8ff1545ae0133365d
shyravina19102004 <shyravina19102004@gmail.com>
2026-04-12 23:11:58 +0300

    1

--- team_finder/settings.py
+++ team_finder/settings.py
@@ -1,27 +1,18 @@
 from pathlib import Path
 from decouple import config
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-<<<<<<< HEAD
-SECRET_KEY = config("DJANGO_SECRET_KEY")
-DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = []
-
-# Application definition
-=======
-# TODO: Создать и заполнить .env, ориентируясь на .env_example
-
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
->>>>>>> 4e5422aae3382a86db5ad8c67f60db6d95aab449
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -29,11 +20,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-<<<<<<< HEAD
     "team_finder.accounts",
     "team_finder.projects",
-=======
->>>>>>> 4e5422aae3382a86db5ad8c67f60db6d95aab449
 ]
 
 MIDDLEWARE = [
@@ -51,11 +39,7 @@ ROOT_URLCONF = "team_finder.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-<<<<<<< HEAD
         "DIRS": [BASE_DIR / f"templates_var{config('TASK_VERSION', default='2')}"],
-=======
-        "DIRS": [BASE_DIR / f"templates_var{config('TASK_VERSION', default='1')}"],
->>>>>>> 4e5422aae3382a86db5ad8c67f60db6d95aab449
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -67,7 +51,7 @@ TEMPLATES = [
     },
 ]
 
-<<<<<<< HEAD
+# Database – SQLite (для быстрого локального запуска)
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -75,50 +59,26 @@ DATABASES = {
     }
 }
 
-# Password validation
-=======
-WSGI_APPLICATION = "team_finder.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("POSTGRES_DB"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("POSTGRES_HOST", default="localhost"),
-        "PORT": config("POSTGRES_PORT", default=5432, cast=int),
-    }
-}
-
+# Если вы хотите использовать PostgreSQL (как в задании), раскомментируйте блок ниже
+# и закомментируйте блок с SQLite. Также добавьте переменные в .env:
+# POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT
+#
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": config("POSTGRES_DB"),
+#         "USER": config("POSTGRES_USER"),
+#         "PASSWORD": config("POSTGRES_PASSWORD"),
+#         "HOST": config("POSTGRES_HOST", default="localhost"),
+#         "PORT": config("POSTGRES_PORT", default=5432, cast=int),
+#     }
+# }
 
 # Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
->>>>>>> 4e5422aae3382a86db5ad8c67f60db6d95aab449
 AUTH_PASSWORD_VALIDATORS = []
 if not DEBUG:
     AUTH_PASSWORD_VALIDATORS.extend(
         [
-<<<<<<< HEAD
-            {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-            {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-            {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-            {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
-        ]
-    )
-
-LANGUAGE_CODE = "ru-RU"
-TIME_ZONE = "UTC"
-USE_I18N = True
-USE_TZ = True
-
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-=======
             {
                 "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
             },
@@ -135,34 +95,21 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
     )
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
+LANGUAGE_CODE = "ru-RU"
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-# Media files
->>>>>>> 4e5422aae3382a86db5ad8c67f60db6d95aab449
 
+# Media files (загруженные аватары)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-<<<<<<< HEAD
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-AUTH_USER_MODEL = "accounts.User"
-=======
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
->>>>>>> 4e5422aae3382a86db5ad8c67f60db6d95aab449
+
+# Пользовательская модель (задана в accounts/models.py)
+AUTH_USER_MODEL = "accounts.User"
--- team_finder/urls.py
+++ team_finder/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -14,14 +13,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
-from django.contrib import admin
-from django.shortcuts import redirect
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-
-]
->>>>>>> 4e5422aae3382a86db5ad8c67f60db6d95aab449
